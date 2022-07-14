e85bdc17140e9afbd1a500c36899061bbc93866c
vvarona <vvaronad@gmail.com>
2022-07-14 19:15:23 +0200

    add reset button

--- js/main.js
+++ js/main.js
@@ -4,50 +4,100 @@ const button = document.querySelector('.js-bunton');
 const clueText = document.querySelector('.js-clue');
 const countText = document.querySelector('.js-count');
 const winText = document.querySelector('.js-win');
+const form = document.querySelector('.js-form');
+const resetButton = document.querySelector('.js-reset');
+const printClueText = clueText;
+
+function printTextInHTML(element, text){
+    element.innerHTML = text
+};
 
 
+function printInClueText(text){
+    printTextInHTML(clueText, text);
+};
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 };
 
-function comprobNumbr(){
-    const numberValue = parseInt(numberInput.value);
-    if (numberValue === secretNumber){
-        console.log('Has ganado campeona!!!');
-        clueText.innerHTML = 'Has ganado campeona!!!';
-    } else if (numberValue > secretNumber) {
-        console.log('Demasiado alto');
-        clueText.innerHTML = 'Demasiado alto';
-    } else if (numberValue < secretNumber) {
-        console.log('Demasiado bajo');
-        clueText.innerHTML = 'Demasiado bajo';
-    }
-}
-
-function invalidNumber(){
-    const numberValue = parseInt(numberInput.value);
-    if (numberValue > 100){
-        clueText.innerHTML = 'El número debe estar entre 1 y 100';
-    } else if (numberValue < 0){
-        clueText.innerHTML = 'El número debe estar entre 1 y 100';
-    } else {
-        comprobNumbr();
-    }
-}
-
-const secretNumber = getRandomNumber(100);
+let secretNumber = getRandomNumber(100);
 console.log(secretNumber);
 
+function checkNumber(numberValue){
+    if (numberValue === secretNumber){
+        printInClueText('Has ganado campeona!!!');
+    } 
+    else if (numberValue > secretNumber) {
+        printInClueText('Demasiado alto');
+    } 
+    else if (numberValue < secretNumber) {
+        printInClueText('Demasiado bajo');
+    }
+}
+
+//printInClueText(comprobNumbr());
+
+function isValidNumber(numberValue){
+
+    const isANumber = !Number.isNaN(numberValue);
+    const isWithinLimits = numberValue <= 100 && numberValue >= 0;
+
+    return isANumber && isWithinLimits;
+    
+}
 
 let count = 0;
-function counter() {
-    countText.innerHTML = 'Intentos: ' + (++count);
+
+function addToCounter(){
+    ++count;
+}
+
+function printCounter() {
+    printTextInHTML(countText, 'Número de intentos: ' + (count));
+}
+
+function resetCounter() {
+    count = 0;
+    printCounter();
+}
+
+function resetGame() {
+    resetCounter();
+    secretNumber = getRandomNumber(100);
+}
+
+function resetInput() {
+    numberInput.value = ''
+}
+
+function resetClue() {
+     const printClueText = clueText.textContent;
+    printInClueText(pritnClueText);
 }
 
 button.addEventListener('click', (ev) => {
     ev.preventDefault;
-    invalidNumber();
-    counter();
-    countText.innerHTML = 'Intentos: ' + count ;
+    const numberValue = (numberInput.value!==''? Number(numberInput.value) : Number.NaN);
+    if (isValidNumber(numberValue)){
+        checkNumber(numberValue);
+        addToCounter();
+        printCounter();
+    } else {
+        printInClueText('Error. Debes introducir sólo números entre 0 y 100.')
+    }
+    
 });
+
+
+
+resetButton.addEventListener("click", (ev) => {
+    ev.preventDefault;
+    resetGame();
+    resetInput();
+    resetClue();
+} )
+
+form.addEventListener("click", function(event){
+    event.preventDefault()
+  });
